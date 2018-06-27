9e277e380531d32000b40f56954661b5db9b5be5
brendan <brconrad@symitar.com>
2018-06-27 08:36:36 -0700

    removed intake dependency

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 // ****************************************************************************************************
 
 var dictionary = require('./dictionary');
-var intake = new (require('intake'))();
 
 (function () {
 
